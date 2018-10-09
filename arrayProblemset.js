4fd5457c144d415957987362a92e5ab5f754dd62
Sarath <sarathiscookie@hotmail.com>
2018-10-09 15:05:40 +0530

    es2015: Array max, sum, reverse.

---
+++ arrayProblemset.js
@@ -0,0 +1,19 @@
+/**
+ * Created by php on 05-Apr-18.
+ */
+console.log("Array in reverse order");
+
+function arrReverse(arr) {
+    //console.log(arr.length-1);
+    for(var i = arr.length; i > 0; i--) {
+        console.log(i);
+    }
+}
+
+arrReverse([1,2,3,4,5]);
+
+console.log("Maximum value in array");
+
+console.log("Sum of array");
+
+console.log("Uniform");
