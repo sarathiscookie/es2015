07b353b416051491048ec077b8b5477733c62e98
sarathts <sarathiscookie@hotmail.com>
2019-03-25 22:01:18 +0530

    es2015: Adding Objects

---
+++ addObject.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Title</title>
+</head>
+<body>
+<div class="container">
+	<h1>Add Object</h1>
+</div>
+<script src="addObject.js?v=<?php echo date('YmdHis'); ?>"></script>
+</body>
+</html>
