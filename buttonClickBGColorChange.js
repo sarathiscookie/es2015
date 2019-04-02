f2f537d97c9752fa986042c9009c59a9a1063ba0
sarath <sarath@cabin-holiday.com>
2019-04-02 10:54:28 +0200

    button click background colour change randomnly

--- buttonClickBGColorChange.js
+++ buttonClickBGColorChange.js
@@ -1,11 +1,19 @@
 var button = document.querySelector('button');
-button.onclick = function() {
-    createColor(255);
-};
 
-function createColor(num)
+function random(num)
 {
-    var math        = Math.floor(Math.random()*(num+1));
-    var randomColor = 'rgb(' + math + ',' + math + ',' + math + ')';
+    return Math.floor(Math.random()*(num+1));
+}
+
+function createColor()
+{
+    var randomColor = 'rgb(' + random(255) + ',' + random(255) + ',' + random(255) + ')';
     return document.body.style.backgroundColor = randomColor;
 }
+
+button.onclick = createColor;
+
+/*
+setInterval(function(){
+    createColor();
+}, 1000);*/
