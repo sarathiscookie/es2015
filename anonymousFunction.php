a809e20ab380495294ec3076b3c058f34396937d
sarath <sarath@cabin-holiday.com>
2019-04-02 08:57:09 +0200

    Anonymous function for button click

---
+++ anonymousFunction.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Title</title>
+</head>
+<body>
+<button>SUBMIT</button>
+<script src="anonymousFunction.js"></script>
+</body>
+</html>
