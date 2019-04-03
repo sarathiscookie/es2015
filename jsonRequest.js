8d6e0a6a800a6e84992b4ec63407c9bd6ace7a73
sarath <sarath@cabin-holiday.com>
2019-04-03 14:12:37 +0200

    json request and response

---
+++ jsonRequest.js
@@ -0,0 +1,14 @@
+var url = 'http://localhost/es2015/json/data.json';
+
+var request = new XMLHttpRequest();
+
+request.open('GET', url);
+
+request.responseType = 'json';
+
+request.send();
+
+request.onload = function() {
+    var response = request.response;
+    console.log(response);
+};
