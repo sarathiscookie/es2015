18883c5768fb3063b83c8dad5c93a02803e641cf
sarath <sarath@cabin-holiday.com>
2019-04-04 11:09:36 +0200

    usage of json stringify

---
+++ jsonStringify.js
@@ -0,0 +1,5 @@
+var json = { "name": "Chris", "age": "38" };
+
+var jsonStringify = JSON.stringify(json); // It convert object to string
+
+console.log(jsonStringify);
---
+++ jsonStringify.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Title</title>
+</head>
+<body>
+
+<script src="jsonStringify.js"></script>
+</body>
+</html>
