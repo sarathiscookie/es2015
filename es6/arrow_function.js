413bb74743dd7b31b9317f87701ed0153787a1e7
Sarath TS <sarathiscookie@hotmail.com>
2019-04-22 11:32:47 +0200

    Arrow function

---
+++ es6/arrow_function.js
@@ -0,0 +1,11 @@
+const fun = () => {
+	const data = 'arrow function';
+	return data;
+}
+
+console.log(fun()); // Not have to write anonymous functions. Instead, we can use arrow function
+
+/*Another Method*/
+const shortForm = () => 'Arrow Function';
+
+console.log(shortForm());
