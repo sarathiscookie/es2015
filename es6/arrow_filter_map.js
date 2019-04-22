dcc8c1ce683472f3ca07586c356fcacced334d59
sarathts <sarathiscookie@hotmail.com>
2019-04-22 23:44:57 +0530

    updated functionality

--- es6/arrow_filter_map.js
+++ es6/arrow_filter_map.js
@@ -1,7 +1,9 @@
 const arrays = [2, 3.5, 4, 4.5, 6, -6.5, -7];
 
-const filter = arrays.filter( (arr) => arr > 0 && arr % 2 == 0);
+const fun = (param) => {
+	const result = param.filter( (arr) => arr > 0 && arr % 2 == 0).map( (sqr) => Math.pow(sqr, 2) );
+	return result;
+}
 
-const square = filter.map( (sqr) => Math.pow(sqr, 2) );
 
-console.log(square);
+console.log(fun(arrays));
