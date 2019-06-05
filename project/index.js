8d84f892756eec9b7be432862b4644664d65024c
sarathts <sarathiscookie@hotmail.com>
2019-06-05 23:48:26 +0530

    Project one source code

--- project/index.js
+++ project/index.js
@@ -26,15 +26,22 @@ let app = new function() {
 
         let status = enable.dataset.status;
         let color;
+        let updatedStatus;
+        let buttonColor;
+        let flag = 1;
         
         if(status === '0') {
             color = 'green';
-            customerStatus(1, item, color);
+            updatedStatus = 1;
+            buttonColor = '<button onclick="app.Status('+ item +')" class="button button1 '+color+'">Enable</button>'; 
         }
         else {
             color = 'red';
-            customerStatus(0, item, color);
+            updatedStatus = 0;
+            buttonColor = '<button onclick="app.Status('+ item +')" class="button button1 '+color+'">Disable</button>'; 
         }
+
+        return customerStatus(updatedStatus, item, color, flag, buttonColor);
     }
 
     this.Edit = function(item) {
@@ -47,25 +54,39 @@ let app = new function() {
 
 }
 
-function appendValues(company, contact, country, status, increment)
+function random(num)
 {
+    return Math.floor(Math.random()*(num+1));
+}
+
+function appendValues(company, contact, country, status, increment)
+{ 
     let color = (status === 0) ? 'red' : 'green';
     const html = '<tr>'+
     '<td><input type="checkbox"></td>'+
     '<td>'+company+'</td>'+
     '<td>'+contact+'</td>'+
     '<td>'+country+'</td>'+
-    '<td class="enabledisablestatus" data-status="'+status+'">'+customerStatus(status, increment, color)+'</td>'+
+    '<td class="enabledisablestatus" data-status="'+status+'">'+customerStatus(status, increment, color, null, null)+'</td>'+
     '<td><button class="button button3 green">Edit</button> <button class="button button3 red">Delete</button></td>'+
     '</tr>';
 
     return html;
 }
 
-function customerStatus(status, increment, color)
+function customerStatus(status, increment, color, flag = null, append = null)
 {
-    console.log('status ' + status +' increment: '+ increment +' color: '+ color);
-    let buttonStatusColor = '<button onclick="app.Status('+ increment +')" class="button button1 '+color+'">Enable</button>'; 
+    console.log(flag +' : '+ append);
+    let buttonStatusColor;
+
+    if(flag === 1) {
+        buttonStatusColor = append; 
+    }
+    else {
+        buttonStatusColor = '<button onclick="app.Status('+ increment +')" class="button button1 '+color+'">Enable</button>';
+    }
+     
+    //console.log('status ' + status +' increment: '+ increment +' color: '+ color);
 
     return buttonStatusColor;
 }
