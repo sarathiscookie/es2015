5580d65f0f17e541e8eabeecd3c1217e27aaf86c
sarathts <sarathiscookie@hotmail.com>
2019-08-31 03:06:23 +0530

    export and import functions in es6

--- es6/import.js
+++ es6/import.js
@@ -1,6 +1,7 @@
 "use strict";
 import { foo, bar } from "./export.js"
-import { uppercaseFun } from "./string_uppercase.js"
+import { uppercaseFun, lowercaseFun } from "./string_uppercase.js"
 console.log(foo);
 console.log(bar);
 console.log(uppercaseFun("hello"));
+console.log(lowercaseFun("HELLO WORLD"));
--- es6/string_uppercase.js
+++ es6/string_uppercase.js
@@ -1,6 +1,12 @@
 "use strict";
-let result;
-function uppercaseFun(str) {
-	result = str.toUpperCase();
-	export { result };
+let uppercase;
+let lowercase;
+export function uppercaseFun(str) {
+	uppercase = str.toUpperCase();
+	return uppercase;
+}
+
+export function lowercaseFun(str) {
+	lowercase = str.toLowerCase();
+	return lowercase;
 }
