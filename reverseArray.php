9b326305b7a1401804f83c7169cd122d28d3ee20
sarathts <sarathiscookie@hotmail.com>
2019-09-26 00:22:56 +0530

    reverse array

---
+++ reverseArray.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Continue And Break</title>
+</head>
+<body>
+
+<script>
+	let userDetails = ['john', 'smith', 1986, 'married', 'usa'];
+
+	for(let i = userDetails.length - 1; i >= 0; i--) {
+		console.log(userDetails[i]);
+	}
+</script>
+</body>
+</html>
