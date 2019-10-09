688e3dde5d7baea1baa9bd1991e45739c4260310
sarathts <sarathiscookie@hotmail.com>
2019-10-09 23:18:19 +0530

    Created three controllers. Budget, UI and combined Budget & UI

--- budgetApp/app.js
+++ budgetApp/app.js
@@ -1,70 +1,20 @@
 //Function for budget
 let budgetController = (function() {
 
-
+	//some code
 
 })();
 
 //Function for UI
 let UIController = (function() {
 
-	//Declared dom strings in object
-	let DOMStrings = {
-		inputType: '.add__type',
-		inputDesc: '.add__description',
-		inputValue: '.add__value',
-		inputButton: '.add__btn'
-	}
-
-	//To access public
-	return {
-		getInput: function() {
-			return {
-				type: document.querySelector(DOMStrings.inputType).value,
-				description: document.querySelector(DOMStrings.inputDesc).value,
-				value: document.querySelector(DOMStrings.inputValue).value
-			}
-		},
-
-		getDOMstrings: function() {
-			return DOMStrings;
-		} 
-	};
-
+	//some code
+	
 })();
 
 //function for combine UI and Budget
 let controller = (function(budgetCtrl, UICtrl) {
 
-	let DOM = UICtrl.getDOMstrings();
-
-	//Added event listeners in to a function
-	let setupEventListeners = function() {
-		document.querySelector(DOM.inputButton).addEventListener('click', ctrlAddItem);
-
-		document.addEventListener('keypress', function(event) {
-			if(event.keyCode === 13 || event.which === 13) {
-				ctrlAddItem();
-			}
-		});
-	};
-
-	let ctrlAddItem = function() {
-		// 1. Get the field input data
-		console.log(UICtrl.getInput());
-		// 2. Add the item to the budget controller 
-		// 3. Add the item to the UI
-		// 4. Calculate the budget
-		// 5. Display the budget on the UI
-	};
-
-	return {
-		init: function() {
-			console.log('Application has started');
-			setupEventListeners();
-		}
-	};
+	//some code
 
 })(budgetController, UIController);
-
-controller.init();
