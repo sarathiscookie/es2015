0072e6000100b4b6f7f3129e6a439d15115baa31
sarathts <sarathiscookie@hotmail.com>
2019-10-09 23:54:13 +0530

    initializing events and calling outside controller

--- budgetApp/app.js
+++ budgetApp/app.js
@@ -36,6 +36,17 @@ let controller = (function(budgetCtrl, UICtrl) {
 
 	let DOM = UICtrl.getDOMstrings();
 
+	//Added event listeners in to a function
+	let setupEventListeners = function() {
+		document.querySelector(DOM.inputButton).addEventListener('click', ctrlAddItem);
+
+		document.addEventListener('keypress', function(event) {
+			if(event.keyCode === 13 || event.which === 13) {
+				ctrlAddItem();
+			}
+		});
+	};
+
 	let ctrlAddItem = function() {
 		// 1. Get the field input data
 		console.log(UICtrl.getInput());
@@ -44,15 +55,15 @@ let controller = (function(budgetCtrl, UICtrl) {
 		// 4. Calculate the budget
 		// 5. Display the budget on the UI
 	};
-	
 
-	document.querySelector(DOM.inputButton).addEventListener('click', ctrlAddItem);
-
-	document.addEventListener('keypress', function(event) {
-		if(event.keyCode === 13 || event.which === 13) {
-			ctrlAddItem();
+	return {
+		init: function() {
+			console.log('Application has started');
+			setupEventListeners();
 		}
-	});
+	};
 
 
 })(budgetController, UIController);
+
+controller.init();
