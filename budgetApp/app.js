5642d7cb2286097fffd20cf557ef43271717dbbd
sarathts <sarathiscookie@hotmail.com>
2019-10-09 23:49:19 +0530

    when click or keypress event, input values are printing on console

--- budgetApp/app.js
+++ budgetApp/app.js
@@ -34,8 +34,25 @@ let UIController = (function() {
 //function for combine UI and Budget
 let controller = (function(budgetCtrl, UICtrl) {
 
-	console.log(UICtrl.getInput());
-	console.log(UICtrl.getDOMstrings());
+	//console.log(UICtrl.getDOMstrings());
+
+	let ctrlAddItem = function() {
+		// 1. Get the field input data
+		console.log(UICtrl.getInput());
+		// 2. Add the item to the budget controller 
+		// 3. Add the item to the UI
+		// 4. Calculate the budget
+		// 5. Display the budget on the UI
+	};
+	
+
+	document.querySelector('.add__btn').addEventListener('click', ctrlAddItem);
+
+	document.addEventListener('keypress', function(event) {
+		if(event.keyCode === 13 || event.which === 13) {
+			ctrlAddItem();
+		}
+	});
 
 
 })(budgetController, UIController);
