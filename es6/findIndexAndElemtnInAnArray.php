a7f163702cf2f1e4a83f540f41e9f1dff37f3075
sarathts <sarathiscookie@hotmail.com>
2019-12-19 05:48:16 +0530

    find index and element of array in es6 and es5

---
+++ es6/findIndexAndElemtnInAnArray.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Map Functions</title>
+</head>
+<body>
+<h4> Map method ES5 </h4>
+<script>
+	let ages = [10, 15, 17, 20, 11, 9];
+	
+	console.log(ages.findIndex(el => el > 18)); // Find index of an element.
+	console.log(ages.find(el => el > 18)); // Find an element in an array.
+</script>
+</body>
+</html>
