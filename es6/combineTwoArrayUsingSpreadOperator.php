570374c2792c09caed9ab7c044b4ee91fc9da2f7
sarathts <sarathiscookie@hotmail.com>
2019-12-19 06:42:21 +0530

    Combine two arrays using spread operator

---
+++ es6/combineTwoArrayUsingSpreadOperator.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Document</title>
+</head>
+<body>
+	<script>
+		
+		let odd = [1, 3, 5, 7, 9];
+		let even = [2, 4, 6, 8];
+		// Lets combine and generate new array
+		let combine = [...odd, 100, 200, 300, ...even];
+		console.log(combine);
+		
+	</script>
+</body>
+</html> 
