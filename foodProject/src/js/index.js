641afe933efeedc6189775a2be616f2e86ce5d41
sarathts <sarathiscookie@hotmail.com>
2020-01-23 21:34:28 +0530

    Project food: Search model implemented

--- foodProject/src/js/index.js
+++ foodProject/src/js/index.js
@@ -1,19 +1,4 @@
-import axios from 'axios';
+import Search from './models/Search';
 
-async function getResult(query) {
-    try {
-        const result = await axios(`https://forkify-api.herokuapp.com/api/search?q=${query}`); // Response is json response. Not need to use .json().
-        console.log(result.data.recipes);
-
-        // OR 
-        /*const result = await fetch(`https://forkify-api.herokuapp.com/api/search?q=${query}`);
-        const jsonResult = await result.json();
-        console.log(jsonResult);*/
-    }
-    catch(error) {
-        console.log(error);
-    }
-    
-}
-
-getResult('pizza');
+const search = new Search('pizza');
+search.getResult();
---
+++ foodProject/src/js/models/Search.js
@@ -0,0 +1,19 @@
+import axios from 'axios';
+
+export default class Search {
+    constructor(query) {
+        this.query = query;
+    }
+
+    async getResult() {
+        try {
+            // Response is json response. Not need to use .json().
+            const api = await axios(`https://forkify-api.herokuapp.com/api/search?q=${this.query}`); 
+            this.result = api.data.recipes;
+            console.log(this.result);
+        }
+        catch(error) {
+            console.log(error);
+        }
+    }   
+}
