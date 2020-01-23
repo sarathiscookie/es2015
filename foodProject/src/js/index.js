ca408c2de9f6b34e7be7586fc979d9bbbb2bb4d2
sarathts <sarathiscookie@hotmail.com>
2020-01-23 22:31:18 +0530

    Project food: Search controller implemented

--- foodProject/src/js/index.js
+++ foodProject/src/js/index.js
@@ -1,4 +1,27 @@
 import Search from './models/Search';
 
-const search = new Search('pizza');
-search.getResult();
+const state = {};
+
+const controlSearch = async () => {
+    // 1: Get query from view. 
+    const query = 'pizza'; //TODO:get value form view page
+
+    if(query) {
+        // 2: New search object and add to state. 
+        state.search = new Search(query);
+
+        // 3: Prepare UI for results
+
+        // 4: Search for recipes
+        await state.search.getResult();
+
+        // 5: Render results on UI
+        console.log(state.search.result);
+
+    }
+};
+
+document.querySelector('.search').addEventListener('submit', e => {
+    e.preventDefault();
+    controlSearch();
+});
