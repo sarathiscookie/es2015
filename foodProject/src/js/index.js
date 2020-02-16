7232351961a3ec50a4e8be86d5e7675ba446b9a0
sarathts <sarathiscookie@hotmail.com>
2020-02-16 21:34:04 +0530

    Project Food: Fetching recipe details from api

--- foodProject/src/js/index.js
+++ foodProject/src/js/index.js
@@ -1,32 +1,41 @@
 import Search from './models/Search';
+import Recipe from './models/Recipe';
 import * as searchView from './views/searchView';
 import { elements, renderLoader, clearLoader } from './views/base';
 
 const state = {};
 
+/**
+ * Search Controller
+ */
 const controlSearch = async () => {
 
     // 1: Get query from view. 
     const query = searchView.getInput();
 
-    if(query) {
+    if (query) {
         // 2: New search object and add to state. 
         state.search = new Search(query);
 
-        // 3: Prepare UI for results
+        // 3: Prepare UI for results.
         searchView.clearInput(); // Clear search input.
-    
         searchView.clearResults(); // Clear search results.
 
-        renderLoader(elements.searchResult); // Loader rendering
+        renderLoader(elements.searchResult); // Loader rendering.
 
-        // 4: Search for recipes
-        await state.search.getResult();
+        try {
+            // 4: Search for recipes
+            await state.search.getResult();
 
-        clearLoader(); // Loader removing
+            clearLoader(); // Clear the loader.
 
-        // 5: Render results on UI
-        searchView.renderResults(state.search.result); 
+            // 5: Render results on UI.
+            searchView.renderResults(state.search.result);
+        }
+        catch (err) {
+            alert('Something went wrong with the search...');
+            clearLoader(); // Clear the loader.
+        }
     }
 
 };
@@ -38,13 +47,49 @@ elements.searchForm.addEventListener('submit', e => {
 
 elements.searchResPages.addEventListener('click', e => {
     const btn = e.target.closest('.btn-inline');
-    
-    if(btn) {
+
+    if (btn) {
         const goToPage = parseInt(btn.dataset.goto, 10);
-
         searchView.clearResults(); // Clear search results.
-
-        // Passing page no and rendering results on UI
-        searchView.renderResults(state.search.result, goToPage); 
+        // Passing page no and rendering results on UI.
+        searchView.renderResults(state.search.result, goToPage);
     }
 });
+
+
+/**
+ * Recipe Controller.
+ */
+const controlRecipe = async () => {
+    // Get ID from URL
+    const id = window.location.hash.replace('#', '');
+
+    if (id) {
+        // Prepare UI for changes.
+
+        // Create new recipe object.
+        state.recipe = new Recipe(id);
+
+        try {
+            // Get recipe data and parse ingrediants
+            await state.recipe.getRecipe();
+
+            // Calculate servings and time.
+            state.recipe.calcTime();
+            state.recipe.calcServings();
+
+            // Render recipe.
+            console.log(state.recipe);
+        }
+        catch (err) {
+            alert('Error processing recipe');
+        }
+    }
+}
+
+/* window.addEventListener('hashchange', controlRecipe);
+   window.addEventListener('load', controlRecipe); */
+
+// OR
+
+['hashchange', 'load'].forEach(event => window.addEventListener(event, controlRecipe));
--- foodProject/src/js/models/Search.js
+++ foodProject/src/js/models/Search.js
@@ -10,7 +10,6 @@ export default class Search {
             // Response is json response. Not need to use .json().
             const api = await axios(`https://forkify-api.herokuapp.com/api/search?q=${this.query}`); 
             this.result = api.data.recipes;
-            //console.log(this.result);
         }
         catch(error) {
             console.log(error);
