d210650d434f787ee4d99a018be3fad6b6a167b0
sarathts <sarathiscookie@hotmail.com>
2020-03-01 23:20:53 +0530

    Project Food: List view functionality.

--- foodProject/src/js/index.js
+++ foodProject/src/js/index.js
@@ -3,6 +3,7 @@ import Recipe from './models/Recipe';
 import List from './models/List';
 import * as searchView from './views/searchView';
 import * as recipeView from './views/reipeView';
+import * as listView from './views/listView';
 import { elements, renderLoader, clearLoader } from './views/base';
 
 const state = {};
@@ -104,6 +105,43 @@ const controlRecipe = async () => {
 
 ['hashchange', 'load'].forEach(event => window.addEventListener(event, controlRecipe));
 
+/**
+ * List Controller.
+ */
+const listController = () => {
+    // Clear rendered html
+    listView.clearShopResults();
+
+    // Create a new list If there is none yet
+    if(!state.list) state.list = new List();
+
+    // Add each ingredient to the list
+    state.recipe.ingredients.forEach(el => {
+        const addItem = state.list.addItem(el.count, el.unit, el.ingredient);
+        listView.renderItem(addItem);
+    });
+};
+
+/* Handle delete and update list item events */
+elements.shopping.addEventListener('click', e => {
+    const id = e.target.closest('.shopping__item').dataset.itemid;
+    
+    // Handle the delete button
+    if (e.target.matches('.shopping__delete, .shopping__delete *')) {
+        // Delete from state
+        state.list.deleteItem(id);
+
+        // Delete from UI
+        listView.deleteItem(id);
+    }
+    else if (e.target.matches('.shopping_count-value')) {
+        
+        // Handle the count update
+        const val = parseFloat(e.target.value, 10);
+        state.list.updateCount(id, val);
+    }
+});
+
 /* Handling recipe button clicks */
 elements.recipe.addEventListener('click', e => {
     if (e.target.matches('.btn-decrease, .btn-decrease *')) {
@@ -118,7 +156,7 @@ elements.recipe.addEventListener('click', e => {
         state.recipe.updateServings('inc');
         recipeView.updateServingsIngredients(state.recipe);
     }
+    else if (e.target.matches('.recipe__btn--add, .recipe__btn--add *')) {
+        listController();
+    }
 });
-
-
-window.l = new List();
