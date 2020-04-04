5e5d126e261b71e57e82e875d11ab36397fec70b
sarathts <sarathiscookie@hotmail.com>
2020-04-04 23:40:48 +0530

    Project E-commerce API: Setup UI.

--- ecommerce/src/js/index.js
+++ ecommerce/src/js/index.js
@@ -1,5 +1,5 @@
 
-import '../css/tailwind.css';
+import '../css/style.css';
 console.log('Application loaded.....');
 
 //http://webservice.rakuten.de/merchants/orders/getOrders?key=d6872f58e1ac5c8af686562c6e882ba4
